19208ded000928de2970c1adbefc93f5fb9f19bb
Zach Dicklin <zach@workbright.com>
2017-07-14 10:12:24 -0600

    Docker go build; Martini hello world

--- api/app/api.go
+++ api/app/api.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/go-martini/martini"
+)
 
 func main() {
 	fmt.Println("Hi!")
+	m := martini.Classic()
+	m.Get("/", func() string {
+		return "Hello World!"
+	})
+	m.Run()
 }
